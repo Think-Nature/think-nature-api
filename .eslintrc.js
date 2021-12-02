9d146818dbc4552bb210366eece3d7f59432c9bb
Cedric Ong <cedricong99@gmail.com>
2021-12-02 23:04:54 +0800

    Add linting, sequelise and bcyrpt

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   settings: {},
   extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
   rules: {
+    '@typescript-eslint/no-var-requires': 'off',
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
   },
 };
